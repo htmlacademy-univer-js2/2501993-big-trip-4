7477e40299dcb012f6e262e127f48b98b8e2deb0
Digfall <111002239+Digfall@users.noreply.github.com>
2024-05-07 14:27:55 +0500

    Большие перемены (часть 2)

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,17 +1,21 @@
 import SortingView from '../view/sorting-view.js';
+import { remove, render, RenderPosition} from '../framework/render.js';
 import { render, RenderPosition} from '../framework/render.js';
 import TripPointListView from '../view/trip-point-list-view.js';
 import NoTripPointView from '../view/no-trip-point-view.js';
 import TripPointPresenter from './trip-point-presenter.js';
-import { updateItem } from '../utils.js';
+import { sorting, updateItem } from '../utils.js';
+import { SortType } from '../utils.js';
 
 export default class BoardPresenter {
   #tripContainer = null;
   #tripPointsModel = null;
   #tripPoints = null;
 
+  #currentSortType = SortType.DAY;
+
   #tripPointsList = new TripPointListView();
-  #sortComponent = new SortingView();
+  #sortComponent = null;
   #noTripPointComponent = new NoTripPointView();
 
   #tripPointPresenter = new Map();
@@ -20,6 +24,10 @@ export default class BoardPresenter {
     this.#tripContainer = tripContainer;
     this.#tripPointsModel = tripPointsModel;
   }
+
+  #sortTripPoint = (sortType) => {
+    sorting[sortType](this.#tripPoints);
+  };
 
   init () {
     this.#tripPoints = [...this.#tripPointsModel.tripPoints];
@@ -43,6 +51,9 @@ export default class BoardPresenter {
   };
 
   #renderSort = () => {
+    this.#sortTripPoint(this.#currentSortType);
+    this.#sortComponent = new SortingView(this.#currentSortType);
+    this.#sortComponent.setSortTypeChangeHandler(this.#handleSortTypeChange);
     render(this.#sortComponent, this.#tripContainer, RenderPosition.AFTERBEGIN);
   };
 
@@ -70,5 +81,15 @@ export default class BoardPresenter {
   #renderTripPointList = () => {
     render(this.#tripPointsList, this.#tripContainer);
     this.#renderTripPoints(0, this.#tripPoints.length);
+  };
+  #handleSortTypeChange = (sortType) => {
+    if (this.#currentSortType === sortType) {
+      return;
+    }
+    this.#currentSortType = sortType;
+    remove(this.#sortComponent);
+    this.#renderSort();
+    this.#clearTripPointList();
+    this.#renderTripPointList();
   };
 }
